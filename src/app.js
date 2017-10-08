6ba4d46d158febd96fe461def8a45d2436c38ad6
shruti.arya <you@example.com>
2017-10-08 10:12:45 +0530

    sending data to blockchain

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 
 
-(function () {
-/*
-    function get_snap() {
-
-    }*/
-
-
-
     var config = {
         apiKey: "AIzaSyAUVdiBQHOSYDHLl1R7WXJvv-DcMyvygxk",
         authDomain: "poojab26-firebase.firebaseapp.com",
@@ -31,39 +23,42 @@
 
     var another2 = firebase.database().ref("fir/record-3");
 
-    do {
-        another1.on("value", function (snapshot) {
-            time_arr1 = snapshot.child("report_timestamp").val();
-            s1 = snapshot.val();
-            console.log(time_arr1);
-            console.log(s1);
+function get_values() {
+    another1.on("value", function (snapshot) {
+        time_arr1 = snapshot.child("report_timestamp").val();
+        s1 = snapshot.val();
+        console.log(time_arr1);
+        console.log(s1);
+    });
+
+    another.on("value", function (snapshot) {
+        time_arr2 = snapshot.child("report_timestamp").val();
+        s2 = snapshot.val();
+        console.log(time_arr2);
+    });
+
+    another2.on("value", function (snapshot) {
+        time_arr3 = snapshot.child("report_timestamp").val();
+        s3 = snapshot.val();
+        console.log(time_arr3);
+    });
+
+}
+
+get_values();
+
+    if (time_arr1 === time_arr3) {
+        const rootRef = firebase.database().ref('fir');
+        const storesRef = rootRef.child('record-3');
+        const newStoreRef = storesRef.push();
+
+        newStoreRef.set({
+            'block_numb': '1'
         });
 
-        another.on("value", function (snapshot) {
-            time_arr2 = snapshot.child("report_timestamp").val();
-            s2 = snapshot.val();
-            console.log(time_arr2);
-        });
-
-        another2.on("value", function (snapshot) {
-            time_arr3 = snapshot.child("report_timestamp").val();
-            s3 = snapshot.val();
-            console.log(time_arr3);
-        });
     }
-    while (s1!==null&&s2!==null&&s3!==null) {
-        if (time_arr1 === time_arr2) {
-            const rootRef = firebase.database().ref('fir');
-            const storesRef = rootRef.child('record-2');
-            const newStoreRef = storesRef.push();
-
-            newStoreRef.set({
-                'block_numb': '2'
-            });
 
 
-        }
-    }
     // if(time_arr2==time_arr3){
     //     var rootRef = firebase.database().ref();
     //     var storesRef = rootRef.child('fir/block-2');
@@ -71,4 +66,3 @@
     //     newStoreRef.set(s1);
     // }
 
-}());
