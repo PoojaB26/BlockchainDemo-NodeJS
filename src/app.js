608ea2e2d41d553c83cbf0d59979080c01cbc379
shruti.arya <you@example.com>
2017-10-08 08:12:16 +0530

    sending data to blockchain

--- src/app.js
+++ src/app.js
@@ -1,56 +1,74 @@
+
+
 (function () {
+/*
+    function get_snap() {
+
+    }*/
+
+
+
     var config = {
-        apiKey: "AIzaSyA5_tHWBXwWacE-ec0FVWn5VE_t7rC4w6o",
-        authDomain: "hackathon-firebase-99.firebaseapp.com",
-        databaseURL: "https://hackathon-firebase-99.firebaseio.com",
-        projectId: "hackathon-firebase-99",
-        storageBucket: "hackathon-firebase-99.appspot.com",
-        messagingSenderId: "937205230131"
+        apiKey: "AIzaSyAUVdiBQHOSYDHLl1R7WXJvv-DcMyvygxk",
+        authDomain: "poojab26-firebase.firebaseapp.com",
+        databaseURL: "https://poojab26-firebase.firebaseio.com",
+        projectId: "poojab26-firebase",
+        storageBucket: "poojab26-firebase.appspot.com",
+        messagingSenderId: "164631083677"
     };
     firebase.initializeApp(config);
-    const preObject= document.getElementById("object");
-    const dbRefObject=firebase.database().ref().child('object');
-    dbRefObject.on('value', snap => console.log(snap.val()));
+
+    var flag=0;
+
+    var count=0;
+    var time_arr1,time_arr2,time_arr3;
+    var max=count;
+
+    var s1,s2,s3;
+    var another1 = firebase.database().ref("fir/record-1");
+
+    var another = firebase.database().ref("fir/record-2");
+
+    var another2 = firebase.database().ref("fir/record-3");
+
+    do {
+        another1.on("value", function (snapshot) {
+            time_arr1 = snapshot.child("report_timestamp").val();
+            s1 = snapshot.val();
+            console.log(time_arr1);
+            console.log(s1);
+        });
+
+        another.on("value", function (snapshot) {
+            time_arr2 = snapshot.child("report_timestamp").val();
+            s2 = snapshot.val();
+            console.log(time_arr2);
+        });
+
+        another2.on("value", function (snapshot) {
+            time_arr3 = snapshot.child("report_timestamp").val();
+            s3 = snapshot.val();
+            console.log(time_arr3);
+        });
+    }
+    while (s1!==null&&s2!==null&&s3!==null) {
+        if (time_arr1 === time_arr2) {
+            const rootRef = firebase.database().ref('fir');
+            const storesRef = rootRef.child('record-2');
+            const newStoreRef = storesRef.push();
+
+            newStoreRef.set({
+                'block_numb': '2'
+            });
 
 
-
-
-
-    // return firebase.database().ref('block-'+i).once('value').then(function (snapshot) {
-    //     var a_name = (snapshot.val() && snapshot.val().a_name) || 'Anonymous';
-    //     console.log("block-"+i)
-    //     console.log(a_name);
-    //     // var c_name = (snapshot.val() && snapshot.val().c_name) || 'Anonymous';
-    //     //
-    //     // var c_address = (snapshot.val() && snapshot.val().c_address) || 'Anonymous';
-    //     // console.log(c_address);
-    //     var desc = (snapshot.val() && snapshot.val().desc) || 'Anonymous';
-    //     console.log(desc);
-    //     // var offence_location = (snapshot.val() && snapshot.val().offence_location) || 'Anonymous';
-    //     // var offence_timestamp = (snapshot.val() && snapshot.val().offence_timestamp) || 'Anonymous';
-    //     // var offence_location = (snapshot.val() && snapshot.val().sections) || 'Anonymous';
-    //     // var sections = (snapshot.val() && snapshot.val().sections) || 'Anonymous';
-    //
-    // });
-
-
-    var commentsRef = firebase.database().ref().child('fir');
-    commentsRef.on('child_added', snap => {
-     // var name=snap.child("a_name").val();
-     console.log(snap.val());
-        console.log(snap.child('a_name').val());
-
-
-        for (var i=0; i<1; i++)
-            for (var name in snap[i]) {
-            console.log("hey");
-                console.log("block name: "+name);
-                console.log("a_name: "+snap[i][name].a_name);
-                console.log("Desc: "+snap[i][name].desc);
-            }
-    });
-
-
-
+        }
+    }
+    // if(time_arr2==time_arr3){
+    //     var rootRef = firebase.database().ref();
+    //     var storesRef = rootRef.child('fir/block-2');
+    //     var newStoreRef = storesRef.push();
+    //     newStoreRef.set(s1);
+    // }
 
 }());
